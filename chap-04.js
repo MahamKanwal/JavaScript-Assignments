2cf83dec00f394e8e507c5c7bdca7d609fc16095
Maham Kanwal <kanwalmahsm@gmail.com>
2024-04-22 13:44:55 +0500

    done

---
+++ chap-04.js
@@ -0,0 +1,20 @@
+            // Chapter 4 VARIABLE NAMES: LEGAL & ILLEGAL 
+
+// 1. Declare 3 variables in one statement
+
+
+
+// 2. Declare 5 legal & 5 illegal variable names.
+
+
+// 3. Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______, ______ and ______.For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or _____. For example $name, _name or name
+// d) Variable names are case _________
+// e) Variable names should not be JS _________
+
+
+
+
+// ----------- End of Chapter 4 ----------
