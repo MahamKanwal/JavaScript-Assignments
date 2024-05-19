89d055bbb381d1be15547ef1b2202a031f959b92
Maham Kanwal <kanwalmahsm@gmail.com>
2024-05-20 00:27:40 +0500

    complete

--- chap-12-13/app.js
+++ chap-12-13/app.js
@@ -1,13 +1,51 @@
+// ----------- Chapter 12-13 IF…ELSE & ELSE IF STATEMENT, TESTING SET OF CONDITIONS ----------
+
 // 1. Write a program that takes a character (number or string)
 // in a variable & checks whether the given input is a
 // number, uppercase letter or lower case letter. (Hint: ASCII
 // codes:- A=65, Z=90, a=97, z=122).
+
+
+
+
 // 2. Write a JavaScript program that accept two integers and
 // display the larger. Also show if the two integers are equal.
+
+// var firstNumber = +prompt("Enter first number");
+// var secondNumber = +prompt("Enter second number");
+// if(firstNumber>secondNumber){
+//     alert(firstNumber + " is larger than " + secondNumber);
+// }
+// else if(firstNumber<secondNumber){
+//     alert(secondNumberNumber + " is larger than " + firstNumberNumber);
+// }
+// else{
+//     alert(firstNumber +"and" + secondNumber + "are both equal")
+// }
+
 // 3. Write a program that takes input a number from user &
 // state whether the number is positive, negative or zero.
+
+// var num = +prompt("Please enter number to check whether positive, negative or zero");
+// if (num>0){
+//     alert (num+" is positive");
+// }
+// else if (num<0){
+//     alert(num+" is negative");
+// }
+// else{
+//     alert(num+" is zero");
+// }
+
 // 4. Write a program that takes a character (i.e. string of
 // length 1) and returns true if it is a vowel, false otherwise
+
+
+
+
+
+
+
 
 // 5. Write a program that
 // a. Store correct password in a JS variable.
@@ -20,9 +58,17 @@
 // entered matches the original password”. Show
 // “Incorrect password” otherwise.
 
-// IF...ELSE & ELSE IF STATEMENT, TESTING SET OF CONDITION | JAVASCRIPT
-
-// Page 2 of 3
+// var password='mahamkanwal09';
+// var userPassword=prompt('Enter your password');
+// if(userPassword===" "){
+//     alert('Please enter your password');
+// }
+// else if(userPassword===password){
+//      alert('Correct! The password you entered matches the original password.')
+// }
+// else{
+//     alert('Incorrect password')
+// }
 
 // 6. This if/else statement does not work. Try to fix it:
 // var greeting;
@@ -33,6 +79,24 @@
 // greeting = "Good evening";
 // }
 
+// var greeting;
+// var hour=13;
+// if(hour<18){
+//     greeting='Good day'
+//     alert(greeting);
+// }
+// else{
+//     greeting='Good evening';
+//     alert(greeting);
+// }
+
 // 7. Write a program that takes time as input from user in 24
 // hours clock format like: 1900 = 7pm. Implement the
 // following case using if, else & else if statements
+
+
+
+
+
+
+// ----------- End of Chapter 12-13 ----------
