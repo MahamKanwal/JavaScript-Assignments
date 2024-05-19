6f0ecebc836cd914a4f1f24959254be6311cd657
Maham Kanwal <kanwalmahsm@gmail.com>
2024-05-19 14:44:46 +0500

    added

---
+++ chap-09 to 11/app.js
@@ -0,0 +1,143 @@
+// 1. Write a program to take “city” name as input from user. If
+// user enters “Karachi”, welcome the user like this:
+// “Welcome to city of lights”
+
+// var cityName = prompt("Enter the city name") ;
+// if(cityName === "karachi"){
+// alert("Welcome to city of lights");
+// }
+
+// 2. Write a program to take “gender” as input from user. If the
+// user is male, give the message: Good Morning Sir. If the
+// user is female, give the message: Good Morning Ma’am.
+
+// var userGender = prompt ("Enter your gender");
+// if (userGender === "male"){
+//     alert("Good Morning Sir");
+// }
+// else if (userGender === "female"){
+//     alert("Good Morning Ma’am");
+// }
+// else {
+//     alert("Good Morning");
+// }
+
+// 3. Write a program to take input color of road traffic signal
+// from the user & show the message according to this table:
+// Signal color Message
+// Red Must Stop
+// Yellow Ready to move
+// Green Move now
+
+// var color = prompt("Enter the traffic color");
+// if (color === "red"){
+//     alert("Must Stop");
+// }
+// else if (color === "yellow"){
+//     alert("Ready to move");
+// }
+// else if (color === "green"){
+//     alert("Move now");
+// }
+
+// 4. Write a program to take input remaining fuel in car (in
+// litres) from user. If the current fuel is less than 0.25litres,
+// show the message “Please refill the fuel in your car”
+
+// var carFuel = prompt ("Enter your remaining fuel in car in litres");
+// if (carFuel < "0.25"){
+//     alert("Please refill the fuel in your car");
+// }
+
+// 5. Run this script, & check whether alert message would be
+// displayed or not. Record the outputs.
+
+// Part A
+//  var a = 4;
+//  if (++a === 5){
+//  alert("given condition for variable a is true");
+//  }
+// Ans. In the above code, the alert message will be displayed.
+
+// b. var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+// c. var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+
+// d. var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+// e. if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+
+// f. if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+
+// 6. Write a program to take input the marks obtained in three
+// subjects & total marks. Compute & show the resulting
+// percentage on your page. Take percentage & compute
+// grade as per following table:
+
+// Show the total marks, marks obtained, percentage, grade &
+// remarks like:
+
+// 7. Guess game:
+// Store a secret number (ranging from 1 to 10) in a variable.
+// Prompt user to guess the secret number.
+// a. If user guesses the same number, show “Bingo! Correct
+// answer”.
+
+// b. If the guessed number +1 is the secret number, show
+// “Close enough to the correct answer”.
+
+// 8. Write a program to check whether the given number is
+// divisible by 3. Show the message to the user if the number
+// is divisible by 3.
+
+// 9. Write a program that checks whether the given input is an
+// even number or an odd number.
+
+// 10. Write a program that takes temperature as input and
+// shows a message based on following criteria
+
+// a. T > 40 then “It is too hot outside.”
+
+// b. T > 30 then “The Weather today is Normal.”
+
+// c. T > 20 then “Today’s Weather is cool.”
+
+// d. T > 10 then “OMG! Today’s weather is so Cool.”
+
+// 11. Write a program to create a calculator for +,-,*, / & %
+// using if statements. Take the following input:
+
+// a. First number
+
+// b. Second number
+
+// c. Operation (+, -, *, /, %)
+
+// Compute & show the calculated result to user.
