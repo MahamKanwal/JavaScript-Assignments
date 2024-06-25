4ecb0c8621e5763412e64a7482a5c27af582a005
Maham Kanwal <kanwalmahsm@gmail.com>
2024-06-25 16:30:59 +0500

    practice

---
+++ Practice/app.js
@@ -0,0 +1,62 @@
+// CHANGING CASE 
+
+// var myName = "MAHAM KANWAL";
+// // myName.toLowerCase()
+// var loweredString = myName.toLowerCase()
+// console.log(loweredString);
+
+// var array1 = [1,2,3]
+// array2 = array1
+// array1.push(4)
+// console.log(array1);
+// console.log(array2);
+
+// var num1 = 2;
+// var num2 = num1;
+// num1 = num1 + 5;
+// console.log(num1);
+// console.log(num2);
+
+// var cities = ["karachi","islamabad","lahore","peshawar","quetta"]
+// var userCity = prompt("Enter your city");
+//  for(var i=0 ; i<cities.length ; i++){
+//     if(cities[i]==userCity){
+//         alert("Your city is found");
+//         break
+//     }
+//  }
+
+// var stuName = "maham";
+// var firstLetter = stuName[0].toUpperCase();
+// console.log(firstLetter);
+// var restLetters = stuName.slice(1).toLowerCase();
+// console.log(restLetters);
+// var result = firstLetter + restLetters;
+// console.log(result);
+
+// var result = stuName[0].toUpperCase() + stuName.slice(1).toLowerCase();
+// console.log(result);
+
+// var studentNames = "maham manahil nazia kanwal";
+// var array1 = studentNames.split(" ")
+// var titleCase = []
+// // console.log(array1);
+// for(var i=0; i<array1.length; i++){
+//     titleCase.push(array1[i][0].toUpperCase() + array1[i].slice(1).toLowerCase())
+// }
+//  var result = titleCase.join(" ");
+//  console.log(result);
+
+//  var fruit = "watermelon";
+//  var fruits = ["mango","banana","pineapple","apple","coconut","chickoo"]
+// console.log(fruit.length);
+// console.log(fruits.length);
+
+// var fruit = "mangos";
+//   var fruits = [ "apple","banana","grapes"]
+//  console.log(fruit.length);
+// console.log(fruits.length);
+
+// var slicedFruit = fruits.slice(-4,-2);
+// console.log(slicedFruit);
+
---
+++ chap-21 to 25/app.js
@@ -0,0 +1,90 @@
+                    // CHAPTER 21 TO 25 STRING METHODS
+
+                    // Task 1
+                    //Write a program that takes two user inputs for first and last name using prompt and merge them in a new variable titled fullName. Greet the user using his full name. 
+                    
+                   
+                    
+                    // Task 2
+                    // Write a program to take a user input about his favorite mobile phone model. Find and display the length of user input in your browser.
+                    
+                    
+                    
+                    // Task 3
+                    //Write a program to find the index of letter “n” in the word “Pakistani” and display the result in your browser.
+                    
+                    
+
+                    // Task 4
+                    // Write a program to find the last index of letter “l” in the word “Hello World” and display the result in your browser.
+                    
+                    
+                    
+                    //Task 5
+                    // Write a program to find the character at 3rd index in the word “Pakistani” and display the result in your browser.
+                    
+                    
+                    
+                    //Task 6
+                    // Repeat Q1 using string concat() method.
+                    
+
+                    
+                    //Task 7
+                    // Write a program to replace the “Hyder” to “Islam” in the word “Hyderabad” and display the result in your browser.
+                    
+
+
+                    //Task 8
+                    //  Write a program to replace all occurrences of “and” in the string with “&” and display the result in your browser.
+                    
+                    
+                    
+                    // Task 9
+                    // Write a program that converts a string “472” to a number 472. Display the values & types in your browser.
+                    
+                
+                    
+                    //Task 10
+                    // Write a program that takes user input. Convert and show the input in capital letters.
+                    
+                    
+
+                    //Task 11
+                    // Write a program that takes user input. Convert and show the input in title case.
+                    
+                    
+                    
+                    
+                    // Task 12
+                    // Write a program that converts the variable num to string. var num = 35.36 ; Remove the dot to display “3536” display in your browser. 
+                    
+                    
+                    
+                    //Task 13
+                    //Write a program to take user input and store username in a variable. If the username contains any special symbol among [@ . , !], prompt the user to enter a valid username. For character codes of [@ .
+                    
+                    
+                    
+                    // Task 14
+                    // Write a program to enable “search by user input” in an array. After searching, prompt the user whether the given item is found in the list or not.
+                    
+                    
+                    
+                    // Task 15
+                    // Write a program to take password as an input from user. The password must qualify these requirements: a. It should contain alphabets and numbers b. It should not start with a number c. It must at least 6 characters long.
+                    
+                    // Task 16
+// Write a program to convert the following string to an array using string split method. var university = “University of Karachi”;
+                    
+                    // Task 17
+                    // Write a program to display the last character of a user input. 
+                    
+                    
+                    
+                    // Task 18
+                    // You have a string “The quick brown fox jumps over the lazy dog”. Write a program to count number of occurrences of word “the” in given string.
+                    
+                    
+                    
+                    // ---------------------------End of Chap 21-25
