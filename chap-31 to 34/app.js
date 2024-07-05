3b41b60f4f29d4c74bd98bd5dab04ad3a1534041
Maham Kanwal <kanwalmahsm@gmail.com>
2024-07-05 19:39:24 +0500

    added

---
+++ chap-31 to 34/app.js
@@ -0,0 +1,96 @@
+
+// ----------- Chapter 31-34 Date Methods ----------
+
+// Task 1
+// Write a program that displays current date and time in your browser. 
+
+// var date = new Date();
+// document.write(date)
+
+// Task 2
+// Write a program that alerts the current month in words. For example December.
+
+// var date = new Date();
+// var month = date.getMonth();
+
+// var months = ["January","February","Maarch","April","May","June","July","August","September","October","November","December"];
+// document.write("Current Month : " + months[month]);
+
+// Task 3
+// Write a program that alerts the first 3 letters of the current day, for example if today is Sunday then alert will show Sun.
+
+// var date = new Date();
+// var day = date.getDay();
+
+// var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+// document.write("Current Day : " + days[day]);
+
+// Task 4
+// Write a program that displays a message “It’s Fun day” if its Saturday or Sunday today.
+
+// var date = new Date();
+// var day = date.getDay();
+
+// var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+// if(day === "Sunday" && day === "Sunday"){
+//     alert("It’s Fun day");
+// }
+// else{
+//     alert("Today is not fun day");
+// }
+
+// Task 5
+// Write a program that shows the message “First fifteen days of the month” if the date is less than 16th of the month else shows “Last days of the month”. 
+
+
+
+// Task 6
+//  Write a program that determines the minutes since midnight, Jan. 1, 1970 and assigns it to a variable that hasn't been declared beforehand. Use any variable you like to represent the Date object.
+
+
+
+// Task 7
+// Write a program that tests whether it's before noon and alert “Its AM” else “its PM”. 
+
+
+
+
+// Task 8
+//  Write a program that creates a Date object for the last day of the last month of 2020 and assigns it to variable named laterDate. 
+
+
+
+//     Task 9
+// Create a date object of the starting date of this Ramadan and alert the number of days past since 1st Ramadan? Note: 1st Ramadan was on June 18, 2015 
+
+
+
+// Task 10
+// Write a program that displays in your browser the seconds that elapsed between the reference date and the beginning of 2015.
+
+
+
+// Task 11
+// Create a Date object for the current date and time. Extract the hours, reset the date object an hour ahead and finally display the date object in your browser. 
+
+
+
+// Task 12
+// Write a program that creates a date object and show the date in an alert box that is reset to 100 years back?
+
+
+
+// Task 13
+// Write a program to ask the user about his age. Calculate and show his birth year in your browser. 
+
+
+
+
+
+// Task 14
+// Write a program to generate your K-Electric bill in your browser. All the amounts should be rounded off to 2 decimal places. Display the following fields: a. Customer Name b.
+
+
+
+//----------End of Chap 31-34
